7659323978352917800a659c5a7de9f146f37093
dlovell <dlovell@gmail.com>
2013-08-10 04:45:21 -0700

    install ack-grep

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apt-get update
 apt-get install -y htop nmon sysstat
 
 # support tools
-apt-get install -y screen sshfs xclip
+apt-get install -y screen sshfs xclip ack-grep
 
 # build tools
 apt-get install -y g++ make
