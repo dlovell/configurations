869fb5aa661c2e61de87acdf32f5dcfa93a52965
dlovell <dlovell@gmail.com>
2013-09-28 13:10:41 -0400

    install ubuntu-12.04.2; use dlovell's gist for preseed

--- packer_tools/create_vm_via_packer.sh
+++ packer_tools/create_vm_via_packer.sh
@@ -4,14 +4,14 @@ cat -- >$packer_config_filename <<EOF
 {
 	"builders": [{
 	"type": "vmware",
-	"iso_url": "http://releases.ubuntu.com/13.04/ubuntu-13.04-server-amd64.iso",
-	"iso_checksum": "7d335ca541fc4945b674459cde7bffb9",
+	"iso_url": "http://old-releases.ubuntu.com/releases/precise/ubuntu-12.04.2-server-amd64.iso",
+	"iso_checksum": "af5f788aee1b32c4b2634734309cc9e9",
 	"iso_checksum_type": "md5",
 	"ssh_username": "packer",
 	"ssh_password": "packer",
 	"shutdown_command": "echo packer | sudo -S shutdown -P now",
 	"disk_size": 8000,
-	"boot_command": [ "<esc><esc><enter><wait>", "/install/vmlinuz noapic ", "preseed/url=http://gist.github.com/Josiah/6005339/raw/2a13d3839818848b5d7e802c08aa29986bd5a150/preseed.cfg ", "debian-installer=en_US auto locale=en_US kbd-chooser/method=us ", "hostname={{ .Name }} ", "fb=false debconf/frontend=noninteractive ", "keyboard-configuration/modelcode=SKIP keyboard-configuration/layout=USA ", "keyboard-configuration/variant=USA console-setup/ask_detect=false ", "initrd=/install/initrd.gz -- <enter>" ]
+	"boot_command": [ "<esc><esc><enter><wait>", "/install/vmlinuz noapic ", "preseed/url=http://gist.github.com/dlovell/6574899/raw/c95ea6bcbcc1e56cbba6f53296a516a5d3b4cbb1/ubuntu-12.04.2-server-preseed.cfg ", "debian-installer=en_US auto locale=en_US kbd-chooser/method=us ", "hostname={{ .Name }} ", "fb=false debconf/frontend=noninteractive ", "keyboard-configuration/modelcode=SKIP keyboard-configuration/layout=USA ", "keyboard-configuration/variant=USA console-setup/ask_detect=false ", "initrd=/install/initrd.gz -- <enter>" ]
 }]
 }
 EOF
