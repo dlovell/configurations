0420fd8d380b256a3a7aed799c1802cc06a79714
dlovell <dlovell@gmail.com>
2013-09-28 13:15:28 -0400

    add packer_config to repo instead of creating each time

--- packer_tools/create_vm_via_packer.sh
+++ packer_tools/create_vm_via_packer.sh
@@ -1,20 +1,8 @@
-# create vmware config
-packer_config_filename=vmware_packer_config.json
-cat -- >$packer_config_filename <<EOF
-{
-	"builders": [{
-	"type": "vmware",
-	"iso_url": "http://old-releases.ubuntu.com/releases/precise/ubuntu-12.04.2-server-amd64.iso",
-	"iso_checksum": "af5f788aee1b32c4b2634734309cc9e9",
-	"iso_checksum_type": "md5",
-	"ssh_username": "packer",
-	"ssh_password": "packer",
-	"shutdown_command": "echo packer | sudo -S shutdown -P now",
-	"disk_size": 8000,
-	"boot_command": [ "<esc><esc><enter><wait>", "/install/vmlinuz noapic ", "preseed/url=http://gist.github.com/dlovell/6574899/raw/c95ea6bcbcc1e56cbba6f53296a516a5d3b4cbb1/ubuntu-12.04.2-server-preseed.cfg ", "debian-installer=en_US auto locale=en_US kbd-chooser/method=us ", "hostname={{ .Name }} ", "fb=false debconf/frontend=noninteractive ", "keyboard-configuration/modelcode=SKIP keyboard-configuration/layout=USA ", "keyboard-configuration/variant=USA console-setup/ask_detect=false ", "initrd=/install/initrd.gz -- <enter>" ]
-}]
-}
-EOF
+packer_config_filename=$1
+if [[ -z $packer_config_filename ]]; then
+	echo "USAGE: bash create_vm_via_packer.sh PACKER_CONFIG_FILENAME"
+	exit
+fi
 
 # validate and build
 packer validate $packer_config_filename && packer build $packer_config_filename
