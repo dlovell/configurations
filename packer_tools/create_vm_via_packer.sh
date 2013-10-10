1eb9c38e56e563e4042d846b570f692c71b9c4e3
dlovell <dlovell@gmail.com>
2013-10-10 10:43:01 -0400

    try using nat with specified port for ssh to guest

--- packer_tools/create_vm_via_packer.sh
+++ packer_tools/create_vm_via_packer.sh
@@ -1,7 +1,6 @@
 packer_config_filename=$1
-MAC_ADDRESS=$2
-if [[ -z $MAC_ADDRESS ]]; then
-	echo "USAGE: bash create_vm_via_packer.sh PACKER_CONFIG_FILENAME MAC_ADDRESS"
+if [[ -z $packer_config_filename ]]; then
+	echo "USAGE: bash create_vm_via_packer.sh PACKER_CONFIG_FILENAME"
 	exit
 fi
 
@@ -14,15 +13,14 @@ cd $config_abs_dir
 
 
 # validate and build
-MAC_VAR_STR="-var MAC_ADDRESS=$MAC_ADDRESS"
-packer validate $MAC_VAR_STR $packer_config_filename
+packer validate $packer_config_filename
 if [[ $? -ne 0 ]]; then
 	echo "Failed to validate $packer_config_filename"
 	exit
 fi
 
 
-export PACKER_LOG=1 && packer build $MAC_VAR_STR $packer_config_filename >out 2>err
+export PACKER_LOG=1 && packer build $packer_config_filename >out 2>err
 vm_ip=$(grep Detected err | tail -n 1 | awk '{print $NF}')
 echo "vm_ip=$vm_ip"
 ssh-keygen -f ~/.ssh/known_hosts -R $vm_ip
