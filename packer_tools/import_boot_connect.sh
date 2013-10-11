62bc1acc66c617c4058412ad433467a21127a841
dlovell <dlovell@gmail.com>
2013-10-11 00:51:06 -0400

    add some comments about how to power off and remove VM

--- packer_tools/import_boot_connect.sh
+++ packer_tools/import_boot_connect.sh
@@ -3,3 +3,8 @@ VBoxManage import output-virtualbox.crosscat/packer-virtualbox.ovf
 VBoxManage modifyvm ~/VirtualBox\ VMs/packer-virtualbox/packer-virtualbox.vbox --macaddress1 080027DC1BB1
 VBoxManage startvm ~/VirtualBox\ VMs/packer-virtualbox/packer-virtualbox.vbox
 ssh -p 2222 packer@localhost
+
+# to power off
+# VBoxManage controlvm ~/VirtualBox\ VMs/packer-virtualbox/packer-virtualbox.vbox poweroff
+# to remove (must be powered off)
+# VBoxManage unregistervm ~/VirtualBox\ VMs/packer-virtualbox/packer-virtualbox.vbox --delete
