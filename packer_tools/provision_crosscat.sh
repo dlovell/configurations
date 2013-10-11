045d6a9ed2cbaeb0127f32bad07f337e3a9b24bc
dlovell <dlovell@gmail.com>
2013-10-11 13:43:28 -0400

    general cleanup; make user/pass bigdata/bigdata

--- packer_tools/provision_crosscat.sh
+++ packer_tools/provision_crosscat.sh
@@ -1,32 +1,34 @@
 # install crosscat
-crosscat_dir=/home/packer/crosscat
+
+
+# settings
+crosscat_dir=~/crosscat
+sudo_pass=bigdata
 
 
 # get a subset of ../install.sh
-echo packer | sudo -S apt-get install -y --force-yes git
+echo $sudo_pass | sudo -S apt-get install -y --force-yes git
 # pip, virtualenv, virtualenvwrapper
-echo packer | sudo -S apt-get build-dep -y python-pip
+echo $sudo_pass | sudo -S apt-get build-dep -y python-pip
 # install most recent pip, else virtualenv{,wrapper} installs fail
 wget -qO- https://raw.github.com/pypa/pip/master/contrib/get-pip.py > get-pip.py
-echo packer | sudo -S python get-pip.py
+echo $sudo_pass | sudo -S python get-pip.py
 source ~/.bashrc
-echo packer | sudo -S pip install virtualenv==1.10
-echo packer | sudo -S pip install virtualenvwrapper==3.6
-#
+echo $sudo_pass | sudo -S pip install virtualenv==1.10
+echo $sudo_pass | sudo -S pip install virtualenvwrapper==3.6
 
-echo trying clone
+
 git clone https://github.com/mit-probabilistic-computing-project/crosscat.git $crosscat_dir
-echo cloned crosscat
-echo packer | sudo -S bash $crosscat_dir/scripts/install_scripts/install_ubuntu_packages.sh
-echo ran install_ubuntu_packages.sh
+echo $sudo_pass | sudo -S bash $crosscat_dir/scripts/install_scripts/install_ubuntu_packages.sh
 bash -i $crosscat_dir/scripts/install_scripts/virtualenv_setup.sh
-echo ran virtualenv_setup.sh
+
 
 # build cython + docs
 # source ~/.bashrc
 # workon crosscat
 # make cython
 # make docs
+
 
 # # website install
 # git checkout website_content
