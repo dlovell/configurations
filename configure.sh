82fe5df0897febdb0532a6bc099949721706c60a
dlovell <dlovell@gmail.com>
2014-07-27 20:59:17 -0400

    replace readlink with python_readlink

--- configure.sh
+++ configure.sh
@@ -1,4 +1,10 @@
-my_abs_path=$(readlink -f "$0")
+function python_readlink {
+	PYTHON_COMMAND='import sys, os; print os.path.realpath(sys.argv[1])'
+	PYTHON_OUTPUT=$(python -c "$PYTHON_COMMAND" $1)
+	echo $PYTHON_OUTPUT
+}
+
+my_abs_path=$(python_readlink "$0")
 project_location=$(dirname $my_abs_path)
 cd $project_location
 
