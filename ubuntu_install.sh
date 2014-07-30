0af40d591ab6a91543721edd6a75280821217c0b
dlovell <dlovell@gmail.com>
2014-07-30 13:05:46 -0700

    split conda install into script to be run by user

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -8,12 +8,6 @@ if [[ "$USER" != "root" ]]; then
 fi
 
 
-ANACONDA_URL=http://repo.continuum.io/archive/Anaconda-2.0.1-Linux-x86_64.sh
-MINICONDA_URL=http://repo.continuum.io/miniconda/Miniconda3-3.5.5-Linux-x86_64.sh
-CONDA_URL=$MINICONDA_URL
-WHICH_CONDA=$(basename $CONDA_URL)
-
-
 function apt_get_install {
 	# system monitoring tools
 	apt-get install -y htop nmon sysstat
@@ -36,26 +30,7 @@ function configure_sshd {
 	/etc/init.d/ssh restart
 }
 
-function install_conda {
-	CONDA_DEST=~/$WHICH_CONDA
-	if [[ ! -f $CONDA_DEST ]]; then
-		wget $CONDA_URL -O $CONDA_DEST
-	fi
-	bash $CONDA_DEST -b
-	# get prefix
-	source <(grep -m 1 ^PREFIX= $CONDA_DEST --binary-files=text)
-	echo "
-# added by Miniconda3 3.5.5 installer
-export PATH=\"$PREFIX/bin:\$PATH\"" >>$HOME/.bashrc
-}
-
-funciton conda_install_things {
-	conda install pip patchelf
-}
-
 
 apt-get update
 apt_get_install
 configure_sshd
-install_conda
-conda_install_things
---
+++ user_install_conda.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+ANACONDA_URL=http://repo.continuum.io/archive/Anaconda-2.0.1-Linux-x86_64.sh
+MINICONDA_URL=http://repo.continuum.io/miniconda/Miniconda3-3.5.5-Linux-x86_64.sh
+CONDA_URL=$MINICONDA_URL
+WHICH_CONDA=$(basename $CONDA_URL)
+
+
+function install_conda {
+	CONDA_DEST=~/$WHICH_CONDA
+	if [[ ! -f $CONDA_DEST ]]; then
+		wget $CONDA_URL -O $CONDA_DEST
+	fi
+	bash $CONDA_DEST -b
+	# get prefix
+	source <(grep -m 1 ^PREFIX= $CONDA_DEST --binary-files=text)
+	echo "
+# added by Miniconda3 3.5.5 installer
+export PATH=\"$PREFIX/bin:\$PATH\"" >>$HOME/.bashrc
+}
+
+function conda_install_things {
+	conda install pip patchelf
+}
+
+
+install_conda
+conda_install_things
