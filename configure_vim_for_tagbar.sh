1d1a29f68199613f2dc34bf92ca01b3126943bdc
dlovell <dlovell@gmail.com>
2014-11-07 20:42:47 -0500

    add tagbar for ubuntu

--- configure_vim_for_tagbar.sh
+++ configure_vim_for_tagbar.sh
@@ -9,15 +9,20 @@ VIMRC=~/.vimrc
 function install_syntastic_for_tagbar {
 	TAGBAR_URL=https://github.com/majutsushi/tagbar
 
-	# try a different ctags
-	brew install ctags
 
 	mkdir -p $BUNDLE_DIR && cd $BUNDLE_DIR
 	git clone $TAGBAR_URL
 
+#	brew install ctags
+#	echo "
+#\" use brew's ctags
+#let g:tagbar_ctags_bin='/usr/local/bin/ctags'
+#" >> $VIMRC
+
+	sudo apt-get install exuberant-ctags
 	echo "
-\" use brew's ctags
-let g:tagbar_ctags_bin='/usr/local/bin/ctags'
+\" use exuberant ctags
+let g:tagbar_ctags_bin='/usr/bin/ctags-exuberant'
 " >> $VIMRC
 
 }
