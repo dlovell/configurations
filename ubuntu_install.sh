fb958f6777b54069746fe19b6ec8397528ece6a2
dlovell <dlovell@gmail.com>
2017-04-02 19:56:08 -0400

    ENH: add libgnome2-bin for gnome-open

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -12,7 +12,7 @@ SERVICES=(openssh-server)
 SYSTEM_MONITORING=(htop nmon ncdu powertop cpufrequtils nmap)
 SUPPORT=(sshfs xclip)
 DEVELOPMENT=(git vim-nox tmux ccache)
-GENERAL=(lynx, pidgin)
+GENERAL=(lynx pidgin libgnome2-bin)
 
 
 function apt_get_install {
