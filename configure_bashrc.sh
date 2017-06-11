894c5d0f20f7ce72b12bb5fbd670ae097478432d
dlovell <dlovell@gmail.com>
2017-06-11 12:39:38 -0400

    ENH: add sudo-brightness

--- configure_bashrc.sh
+++ configure_bashrc.sh
@@ -2,4 +2,9 @@ cat >> ~/.bashrc <<EOF
 function tree-less {
 	tree -C \$@ | less -R
 }
+function sudo-brightness {
+	BACKLIGHT_DIR=/sys/class/backlight
+	BACKLIGHT_DIR=\$BACKLIGHT_DIR/\$(ls \$BACKLIGHT_DIR)
+	echo \$1 | sudo tee \$BACKLIGHT_DIR/brightness
+}
 EOF
