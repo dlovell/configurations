6329fb8ed50161429b8efd7ba32e3303b82c02ca
dlovell <dlovell@gmail.com>
2018-02-27 09:21:43 -0500

    ENH: add python checker

--- configure_vim_for_syntastic.sh
+++ configure_vim_for_syntastic.sh
@@ -21,11 +21,13 @@ function install_syntastic_for_pathogen {
 	fi
 	set +u
 	WHICH_FLAKE8=$(source activate $ENV_NAME && which flake8)
+	WHICH_PYTHON=$(source activate $ENV_NAME && which python)
 	set -u
 	echo "
 \" speed up after write by only using flake8 by default
-let syntastic_python_checkers = ['flake8']
+let syntastic_python_checkers = ['flake8', 'python']
 let syntastic_python_flake8_exec = '$WHICH_FLAKE8'
+let syntastic_python_python_exec = '$WHICH_PYTHON'
 
 \" https://github.com/vim-syntastic/syntastic#3-recommended-settings
 let g:syntastic_always_populate_loc_list = 1
