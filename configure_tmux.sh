5bacf28aa114298ac136dc401e85069fd277cf6e
dlovell <dlovell@gmail.com>
2018-04-01 09:46:03 -0400

    HAK: account for change of syntax in tmux 2.4

--- configure_tmux.sh
+++ configure_tmux.sh
@@ -57,7 +57,8 @@ bind -r L resize-pane -R 5
 
 # enable clipboard interaction
 # https://unix.stackexchange.com/a/70798
-bind -t vi-copy y copy-pipe "xclip -sel clip -f | xclip -sel prim"
+# bind -t vi-copy y copy-pipe "xclip -sel clip -f | xclip -sel prim"
+bind -T copy-mode-vi y send-keys -X copy-pipe "xclip -sel clip -f | xclip -sel prim"
 bind C-p run "xclip -o -sel clip | tmux load-buffer -; tmux paste-buffer"
 bind C-P run "xclip -o -sel prim | tmux load-buffer -; tmux paste-buffer"
 EOF
