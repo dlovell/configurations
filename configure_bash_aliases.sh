561ca40f7d0262aba4ffc70c134848c375afbe3c
dlovell <dlovell@gmail.com>
2018-11-30 12:30:25 -0500

    ENH: add conda-create-latest; move alias creation to user_install_conda.sh

--- configure_bash_aliases.sh
+++ configure_bash_aliases.sh
@@ -4,5 +4,4 @@ alias nmap-scan='nmap -sn'
 # nmap-scan \$(get-gateway)/24
 #
 alias tmux-eclimd='tmux new-session -d -s eclimd eclimd'
-alias source-activate-latest='source activate $(conda env list | grep "^anaconda-[0-9]\{8\}" | cut -d" " -f1 | sort | tail -n 1)'
 EOF
