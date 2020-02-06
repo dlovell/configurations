0f350843de744046d2df9f65a75aebb352fcee9f
Dan <dan@altanatech.com>
2020-02-06 13:57:18 -0500

    CLN

--- configure_hub.sh
+++
@@ -1,4 +0,0 @@
-# https://github.com/github/hub/blob/v2.6.1/share/man/man1/hub.1.ronn#https-instead-of-git-protocol
-# conda-clone-latest hub && sactivate hub && conda install --yes hub
-# even after this change, seems like 'hub create' doesn't respect this setting but you can run 'hub init -p' first
-git config --global hub.protocol https
