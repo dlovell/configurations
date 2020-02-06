d125b3d088773989c802a6e1c867b2232ba767b8
Dan <dan@altanatech.com>
2020-02-06 14:23:43 -0500

    wip

--- configure_vim_for_tagbar.sh
+++ configure_vim_for_tagbar.sh
@@ -12,22 +12,10 @@ function install_tagbar_for_pathogen {
 
 	mkdir -p $BUNDLE_DIR && cd $BUNDLE_DIR
 	git clone $TAGBAR_URL
+	sudo yum install --assumeyes ctags
 
-	if [[ ! -z $(uname | grep CYGWIN) ]]; then
-		echo "Detected CYGWIN: Assuming ctags installed"
-	elif [[ ! -z $(uname | grep Darwin) ]]; then
-		echo "Detected Darwin: installing ctags via brew"
-		brew install ctags
-		echo "
-\" use brew's ctags
-let g:tagbar_ctags_bin='/usr/local/bin/ctags'
-" >> $VIMRC
-	elif [[ ! -z $(uname | grep Linux) ]]; then
-		echo "Detected Linux: installing ctags via apt-get"
-		sudo apt-get install exuberant-ctags
-		echo "
 \" use exuberant ctags
-let g:tagbar_ctags_bin='/usr/bin/ctags-exuberant'
+let g:tagbar_ctags_bin='/usr/bin/ctags'
 " >> $VIMRC
 	fi
 
