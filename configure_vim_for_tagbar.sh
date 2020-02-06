2dbb90103492371def0ad8f46cfb9149fc013511
Dan <dan@altanatech.com>
2020-02-06 14:26:16 -0500

    wip

--- configure_vim_for_tagbar.sh
+++ configure_vim_for_tagbar.sh
@@ -12,7 +12,7 @@ function install_tagbar_for_pathogen {
 
 	mkdir -p $BUNDLE_DIR && cd $BUNDLE_DIR
 	git clone $TAGBAR_URL
-	sudo yum install --assumeyes ctags
+	sudo yum install --assumeyes ctags-etags
 
 \" use exuberant ctags
 let g:tagbar_ctags_bin='/usr/bin/ctags'
